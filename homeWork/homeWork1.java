5483376b5234d18f80c8af6ce35ec202ffcc88f4
Aleksandr <alexchembarov@gmail.com>
2023-05-25 17:43:44 +0400

    homeWork

---
+++ homeWork/homeWork1.java
@@ -0,0 +1,9 @@
+package homeWork;
+
+public class homeWork1 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Ознакомиться с рекурсией ! (в контексте сортировок) ");
+    }
+}

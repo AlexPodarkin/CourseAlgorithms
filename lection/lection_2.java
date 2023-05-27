3f84a8df662161ad08035b4502026bfd486ca1a6
Aleksandr <alexchembarov@gmail.com>
2023-05-27 13:45:12 +0400

    homeWork

--- lection/lection_2.java
+++ lection/lection_2.java
@@ -1,25 +1,75 @@
 package lection;
 
-import java.util.Arrays;
+//import java.util.Arrays;
 
 public class lection_2 
 {
     public static void main(String[] args) 
     {
-        int[][] data = 
+
+        int[] array = new int[] { 5, 4, 7, 8, 9, 1, 12, 3, 0, -5, 15 };
+        bubbleSort(array);
+
+        System.out.print("Отсортированный массив ->  ");
+        for (int i = 0; i < array.length; i++) 
         {
-                {},
-                { 1 },
-                { 0, 3, 2, 1 },
-                { 4, 3, 2, 1, 0 },
-                { 6, 8, 3, 123, 5, 4, 1, 2, 0, 9, 7 },
-        };
-        for (int[] arr : data) 
-        {
-            System.out.print(Arrays.toString(arr) + " => ");
-            sort(arr);
-            System.out.println(Arrays.toString(arr));
+            System.out.print(array[i] + " ");
         }
+        System.out.print("\n Бинарный поиск(индекса) числа -> ");
+        System.err.println(binarySearch(array,55));
+        // sort(array);
+        // System.out.println(Arrays.toString(array));
+    }
+
+    public static int binarySearch(int[] array, int value)
+    {
+        return binarySearch(array, value, 0, array.length - 1); //разобрать array.length и 33стр
+    }
+
+    public static int binarySearch(int[] array, int value, int min, int max) 
+    {
+        int midpoint;
+
+        if (max < min) //разобрать max <= min
+        {
+            return -1;
+        } 
+        else 
+        {
+            midpoint = (max - min) / 2 + min;
+        }
+
+        if (array[midpoint] < value) 
+        {
+            return binarySearch(array, value, midpoint + 1, max);
+        } 
+        else if(array[midpoint] > value)
+        {
+            return binarySearch(array, value, min, midpoint - 1);
+        }
+        else
+        {
+            return midpoint;
+        }        
+    }
+
+    public static void bubbleSort(int[] array) 
+    {
+        boolean finish;
+        do 
+        {
+            finish = false;
+            for (int i = 0; i < array.length - 1; i++) 
+            {
+                if (array[i] > array[i + 1]) 
+                {
+                    int temp = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = temp;
+                    finish = true;
+                }
+            }
+        } while (finish);
     }
 
     public static void sort(int[] array) 
